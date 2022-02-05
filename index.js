369901fffb5deca5f8b80563826a524ef8746a06
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-02-06 00:33:25 +0100

    working in word count

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
 //Countring Letters==================================================
 
 // const input = prompt("write your word here")||  "probandoooo";
-const counterLetters = (str)=>{
-        let solution=""
-        let arr = []
-       let count=0;
-    for (let i = 0; i < str.length; i++) {
-        if(str.charAt(i) === str.charAt(i+1)){
-               count ++;
-           }else{
-               count ++
-               arr.push(count+"x"+str.charAt(i))
-               count=0
-           }
-           
-    }
-    for(j=0;j<arr.length;j++){
-        solution+=arr[j]
-    }
-    
-    console.log(solution)
-}
+// const counterLetters = (str)=>{
+//         let solution=""
+//         let arr = []
+//        let count=0;
+//     for (let i = 0; i < str.length; i++) {
+//         if(str.charAt(i) === str.charAt(i+1)){
+//                count ++;
+//            }else{
+//                count ++
+//                arr.push(count+"x"+str.charAt(i))
+//                count=0
+//            }
+
+//     }
+//     for(j=0;j<arr.length;j++){
+//         solution+=arr[j]
+//     }
+
+//     console.log(solution)
+// }
 
 // counterLetters(input)
 
 // Word Count=======================================================
-let input="Hola, esta esta frase frase es de de de pruebaaaa"
+let input = "Hola, esta esta frase frase es de de de pruebaaaa Hola";
 const wordCount = (input) => {
-let arr= input.split("")
- let solution = "";
- let arr2 = [];
- let count = 0;
- for (let i = 0; i < arr.length; i++) {
-     if (arr[i] === arr[i+1]) {
-         count++;
-     } else {
-         count++;
-         arr2.push(count + "x" + arr[i]);
-         count = 0;
-     }
- }
- for (j = 0; j < arr.length; j++) {
-     solution += arr2[j];
- }
+    let arr = input.split(/[ ,\n]+/);
+    let solution = "";
+    let arr2 = [];
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            console.log("POR I",arr[i],"POS J", arr[j])
+            if (arr[i] === arr[j]) {
+                count++;
+            } else if (arr[i] === arr[arr.length - 1]) {
+                arr2.push(arr[i]+"="+count);
+                count = 0;
+            }
+        }
+    }
+    for (j = 0; j < arr.length; j++) {
+        solution += arr2[j];
+    }
 
- console.log(solution);
+    console.log(solution);
 };
 
 wordCount(input);
