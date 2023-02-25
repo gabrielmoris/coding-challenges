80fb45b160f3e8c5c2c5977c5c3aeef807b02dc5
gabriel <gabrielcmoris@gmail.com>
2023-02-25 11:07:48 +0100

    challenge 6 done

--- src/challenges/6/index.ts
+++ src/challenges/6/index.ts
@@ -1,11 +1,35 @@
 import { SolutionFunction } from "../..";
-export const containsDuplicate: SolutionFunction = (arrOfStr): any => {
+export const containsDuplicate: SolutionFunction = (str): any => {
   console.log(
     "\x1b[44m",
     "\x1b[33m",
-    "Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.",
+    "Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string.",
     "\n",
-    "It must be the argument modifyed and not send a new variable",
+    "If there are two or more words that are the same length, return the first word from the string with that length. ",
+    "\n",
+    "Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example 'Hello world123 567'",
     "\x1b[0m"
   );
+  const arrBigWords: string[] = [];
+  const arrOfSentence = str[0].split(" ");
+
+  arrOfSentence.forEach((item) => {
+    const splitted = item.split("");
+    const cleanStr = splitted.map(onlyLetters);
+    arrBigWords.push(cleanStr.join(""));
+  });
+  console.log(arrBigWords);
+
+  arrBigWords.sort(function (a, b) {
+    return b.length - a.length;
+  });
+
+  return arrBigWords[0];
 };
+
+const regex = /[^a-z A-Z]/;
+function onlyLetters(value: string) {
+  if (!regex.test(value)) {
+    return value;
+  }
+}
