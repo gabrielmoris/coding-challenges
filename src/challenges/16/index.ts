9587fc37b6b6da915961d1e75875eb5533388257
gabriel <gabrielcmoris@gmail.com>
2023-03-29 06:49:03 +0200

    * 📝 docs(index.ts): update challenge description and add example * 🐛 fix(index.ts): change function name from deleteNode to maxProfit * ♻️ refactor(index.ts): improve maxProfit function to O(n) complexity
    * 🗑️ chore(challenges/16): delete input.json
    * 🆕 feat(challenges/16): add input.txt
    * 🗑️ chore(challenges/16): delete input2.json

--- src/challenges/16/index.ts
+++ src/challenges/16/index.ts
@@ -3,38 +3,31 @@ import { SolutionFunction } from "../..";
 console.log(
   "\x1b[44m",
   "\x1b[33m",
-  "Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.",
-  "\n",
-  `Symbol       Value
-  I             1
-  V             5
-  X             10
-  L             50
-  C             100
-  D             500
-  M             1000`,
-  "\n",
-  `There is a singly-linked list head and we want to delete a node node in it.
+  `You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 
-  You are given the node to be deleted node. You will not be given access to the first node of head.
+  On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
   
-  All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
-  
-  Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:`,
-  `The value of the given node should not exist in the linked list.
-  The number of nodes in the linked list should decrease by one.
-  All the values before node should be in the same order.
-  All the values after node should be in the same order.`,
+  Find and return the maximum profit you can achieve.`,
+  `Example 1:
+
+  Input: prices = [7,1,5,3,6,4]
+  Output: 7
+  Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+  Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+  Total profit is 4 + 3 = 7.`,
   "\x1b[0m"
 );
-// Checkear bien, no funciona
-export const deleteNode: SolutionFunction = (
-  head: any,
-  nodeToDelete: any
-): any => {
-  let parsedList = JSON.parse(head);
-  let deleteList = JSON.parse(nodeToDelete);
-  deleteList.val = parsedList.next?.val ?? null;
-  deleteList.next = parsedList.next?.next ?? null;
-  return deleteList;
+// Funciona, pero debería buscar hacerlo en O1. Es posible?
+export const maxProfit: SolutionFunction = (prices: string[]): any => {
+  const stockArr = prices.map(toNumber);
+  let profits = 0;
+  for (let i = 1; i < stockArr.length; i++) {
+    if (stockArr[i] > stockArr[i - 1]) {
+      profits = profits + stockArr[i] - stockArr[i - 1];
+    }
+  }
+  return profits;
 };
+function toNumber(value: string) {
+  return Number(value);
+}
