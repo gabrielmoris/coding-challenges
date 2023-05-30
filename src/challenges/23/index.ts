ab2b0c15392f8344bbf2506d1a24836cddbe33a3
gabriel <gabrielcmoris@gmail.com>
2023-05-30 21:45:48 +0200

    solve ListNode merging

--- src/challenges/23/index.ts
+++ src/challenges/23/index.ts
@@ -15,39 +15,69 @@ console.log(
   "\x1b[0m"
 );
 
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
 export const mergeTwoLists: SolutionFunction = (
-  list1: any | null,
-  list2: any
+  nodeList1: any | null,
+  nodeList2: any
 ) => {
   // I try to parse the list or I return an empty object
-  let parsedList1;
-  let parsedList2;
+  let list1;
+  let list2;
 
   try {
-    parsedList1 = JSON.parse(list1);
+    list1 = JSON.parse(nodeList1);
   } catch {
-    parsedList1 = {};
+    list1 = list1;
   }
   try {
-    parsedList2 = JSON.parse(list2);
+    list2 = JSON.parse(nodeList2);
   } catch {
-    parsedList2 = {};
+    list2 = list2;
   }
-  //  I handle edge cases
-  if (!parsedList1.val || !parsedList2.val) {
-    const res = parsedList1.val
-      ? parsedList1
-      : parsedList2.val
-      ? parsedList2
-      : {};
-    return res;
+
+  // I create a new empty node and I asign the head to it
+
+  if (list1 === undefined && list2 === undefined) {
+    return new ListNode();
   }
-  let result;
-  let tail;
-  while (parsedList1.next != null && parsedList2.next != null) {
-    // Finish this
-    // result = { value: parsedList1.next.value, next: tail };
-    // tail = result;
-    // parsedList1 = parsedList1.next;
+
+  if (list1 === undefined) {
+    return list2;
   }
+
+  if (list2 === undefined) {
+    return list1;
+  }
+
+  // It will start with the listnode with value 0, but I just will return the Next
+  let newList = new ListNode();
+  let newListHead = newList;
+
+  while (list1 != null && list2 != null) {
+    // First: if val of list1 is smaller than list2
+    if (list1.val < list2.val) {
+      //I assing the next to the value of the list 1
+      newList.next = new ListNode(list1.val);
+      // I pass to the next item in linked list
+      list1 = list1.next;
+    } else {
+      // If list2.val is smaller I create a new node and I assign the next to this node
+      newList.next = new ListNode(list2.val);
+      // I pass to the next item in linked list
+      list2 = list2.next;
+    }
+    // I pass to the next item in result linked list
+    newList = newList.next;
+  }
+
+  newList.next = list1 || list2;
+  return JSON.stringify(newListHead.next);
 };
