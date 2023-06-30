a69265696481f1aa2479df4ab564b3696bea2902
gabriel <gabrielcmoris@gmail.com>
2023-06-30 20:29:34 +0200

    challenge 30, number of one sin a binary

---
+++ src/challenges/30/index.ts
@@ -0,0 +1,36 @@
+import { SolutionFunction } from "../..";
+
+console.log(
+  "\x1b[44m",
+  "\x1b[33m",
+  `
+  Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+  Note:
+  
+  Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+  In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+   
+  
+  Example 1:
+  
+  Input: n = 00000000000000000000000000001011
+  Output: 3
+  Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+  Example 2:
+  
+  Input: n = 00000000000000000000000010000000
+  Output: 1
+  Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+`,
+  "\x1b[0m"
+);
+
+export const hammingWeight: any = (arg: any) => {
+  let n: number = Number(arg[0]);
+  const binary = (n >>> 0).toString(2);
+  const ones = binary.split("").filter((bit) => {
+    return bit === "1";
+  });
+  return ones.length;
+};
