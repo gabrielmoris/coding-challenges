0a286b0738f09b8a6d8c682b98e2073e7297974c
gabriel <gabrielcmoris@gmail.com>
2023-07-28 20:45:46 +0200

    solve isValid brackets

--- src/challenges/36/index.ts
+++ src/challenges/36/index.ts
@@ -15,8 +15,34 @@ console.log(
 
 export const isValid = (arr: string[]) => {
   const s = arr[0];
+  const callStack: string[] = [];
+  const matches = {
+    "{": "}",
+    "(": ")",
+    "[": "]",
+  };
+  let noClosed = 0;
 
   s.split("").forEach((item) => {
-    //logic here
+    if (callStack.length === 0) {
+      callStack.push(item);
+    } else {
+      noClosed = noClosed + check(callStack, item, matches);
+    }
   });
+  return noClosed === 0 && callStack.length === 0 ? true : false;
+};
+
+const check = (callStack: string[], item: string, matches: any) => {
+  if (item in matches) {
+    callStack.push(item);
+    return 0;
+  } else {
+    if (matches[callStack[callStack.length - 1]] === item) {
+      callStack.pop();
+      return 0;
+    } else {
+      return 1;
+    }
+  }
 };
