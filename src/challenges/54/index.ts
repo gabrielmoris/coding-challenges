0637008bffe88d7afefc87e9cbb0ee22291f9d11
gabriel <gabrielcmoris@gmail.com>
2023-09-27 21:16:19 +0200

    solve using expanding from centers

--- src/challenges/54/index.ts
+++ src/challenges/54/index.ts
@@ -17,5 +17,30 @@ console.log(
 );
 
 export const longestPalindrome = ([s]: string) => {
-  console.log(s);
+  if (s.length <= 1) {
+    return s;
+  }
+  const arrSol = [];
+  for (let i = 0; i < s.length - 1; i++) {
+    // I check depending of:
+    //1: There is no char in the middle and is completelly mirror (abba)
+    arrSol.push(checkPalFromThisChar(i, i, s));
+    // 2: there is a char in the middle (abcba)
+    arrSol.push(checkPalFromThisChar(i, i + 1, s));
+  }
+
+  const possibleSolutions = arrSol.sort((a, b) => {
+    return b.length - a.length;
+  });
+  console.log(possibleSolutions);
+  return possibleSolutions[0];
+};
+
+const checkPalFromThisChar = (left: number, right: number, string: string): string => {
+  while (left >= 0 && right <= string.length - 1 && string[left] === string[right]) {
+    left--;
+    right++;
+  }
+  console.log(left, right);
+  return string.substring(left + 1, right);
 };
