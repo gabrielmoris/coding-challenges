7ddbe4bf7d82b1b144a8377ce13013b6970b8d40
gabriel <gabrielcmoris@gmail.com>
2023-10-14 13:08:33 +0200

    start water trap chalenge

---
+++ src/challenges/59/index.ts
@@ -0,0 +1,14 @@
+console.log(
+  "\x1b[44m",
+  "\x1b[33m",
+  `
+  Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+     
+  https://leetcode.com/problems/trapping-rain-water/
+    `,
+  "\x1b[0m"
+);
+
+export const trap = ({ height }: { height: number[] }) => {
+  return height;
+};
