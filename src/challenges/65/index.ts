aae216478f844fdb8bf0bdc9010b7ba2b0f334ec
gabriel <gabrielcmoris@gmail.com>
2024-07-15 22:32:03 +0200

    still not solved

--- src/challenges/65/index.ts
+++ src/challenges/65/index.ts
@@ -7,17 +7,31 @@ console.log(
   `,
   "\x1b[0m"
 );
-export default function depthFirstSearch(
-  graph: Record<string, Array<string>>,
-  source: string
-): Array<string> {
+export default function depthFirstSearch(graphArg: { default: { [key: string]: string } }, sourceArg: { default: string }): Array<string> {
+  const graph: { [key: string]: string } = graphArg.default;
+  const source: string = sourceArg.default;
   if (Object.keys(graph).length === 0) {
     return [];
   }
 
   let result = new Set();
-  let notVisited = [graph];
+  let notVisited = [source];
 
-  return [""];
+  while (notVisited.length !== 0) {
+    const visiting = notVisited.pop();
+    result.add(visiting);
+
+    if (visiting != undefined) {
+      const linkedNodes: [string] = graph[visiting] as [string];
+
+      linkedNodes.forEach((node) => {
+        if (!result.has(node)) {
+          result.add(node);
+        }
+      });
+    }
+  }
+
+  return Array.from(result) as [string];
 }
 // ['A', 'B', 'E', 'D', 'I', 'J', 'F', 'C', 'G', 'H']
