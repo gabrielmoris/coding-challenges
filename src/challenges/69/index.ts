d4e51962b1f5d7545f73822b57f4d436ac6d7205
gabriel <gabrielcmoris@gmail.com>
2024-08-21 12:45:02 +0200

    start challenge merge-sort

---
+++ src/challenges/69/index.ts
@@ -0,0 +1,18 @@
+console.log(
+  "\x1b[44m",
+  "\x1b[33m",
+  `
+IImplement a function that performs a recursive merge sort. The function should be recursive and takes in an array of integers. The output should be an array with the input sorted in ascending order.
+
+https://www.greatfrontend.com/questions/javascript/merge-sort?list=data-structures-algorithms
+  `,
+  "\x1b[0m"
+);
+
+export default function recursiveMergeSort(arrArg: {
+  default: Array<number>;
+}): Array<number> {
+  const arr = arrArg.default;
+
+  return arr;
+}
---
+++ src/challenges/69/input.ts
@@ -0,0 +1,1 @@
+export default [9, 3, 6, 2, 4, 11];
